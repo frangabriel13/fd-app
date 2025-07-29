6f9565b8b441df4bf43ad288d9352ebe52b82486
Franco <mansilla.franco.1@gmail.com>
2025-07-29 18:25:09 -0300

    cambios

---
+++ app/(tabs)/Shop.tsx
@@ -0,0 +1,12 @@
+import { Text, View } from 'react-native';
+
+const ShopScreen = () => {
+  return (
+    <View>
+      <Text>Esto es la Tienda</Text>
+    </View>
+  );
+};
+
+
+export default ShopScreen;
---
+++ components/footer/Footer.tsx
@@ -0,0 +1,12 @@
+import { Text, View } from 'react-native';
+
+const Footer = () => {
+  return (
+    <View>
+      <Text>Esto es el Footer</Text>
+    </View>
+  );
+};
+
+
+export default Footer;
