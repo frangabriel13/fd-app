910d785ce6e4e6bd72f99487112c14ed3a8c8c83
Franco <mansilla.franco.1@gmail.com>
2025-07-29 18:31:42 -0300

    cambios

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -38,7 +38,7 @@ export default function TabLayout() {
           }}
         />
         <Tabs.Screen
-          name="shop"
+          name="tienda"
           options={{
             title: 'Shop',
             tabBarIcon: ({ color }) => <IconSymbol size={28} name="cart.fill" color={color} />,
