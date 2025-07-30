e478eb96f4748c72775b733cc41e9e345182bd35
Franco <mansilla.franco.1@gmail.com>
2025-07-30 15:42:41 -0300

    cambios

--- components/header/Search.tsx
+++ components/header/Search.tsx
@@ -1,6 +1,9 @@
+import { useState } from 'react';
 import { TextInput, View } from 'react-native';
 
 const Search = () => {
+  const [searchText, setSearchText] = useState('');
+
   return (
     <View className="flex-1 mx-4">
       <View className="flex-row items-center bg-gray-100 rounded px-0 py-0">
@@ -8,7 +11,8 @@ const Search = () => {
           placeholder="Buscar productos o fabricantes..."
           placeholderTextColor="#9ca3af"
           className="flex-1 ml-2 font-mont-regular text-primary"
-          editable={false} // Deshabilitado ya que no tiene funcionalidad
+          value={searchText}
+          onChangeText={setSearchText}
         />
       </View>
     </View> // Flex-1 para ocupar el espacio disponible y mx-4 para márgenes laterales (separación horizontal)
