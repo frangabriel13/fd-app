94c2cf37218cb8421f3aca4066eae6968a070e9e
Franco <mansilla.franco.1@gmail.com>
2025-08-01 16:02:37 -0300

    cambios

--- components/slider/Slider.tsx
+++ components/slider/Slider.tsx
@@ -1,5 +1,6 @@
 import sliderImages from '@/constants/Images';
-import { Dimensions, Image, ScrollView, View } from 'react-native';
+import { useEffect, useRef, useState } from 'react';
+import { Dimensions, Image, NativeScrollEvent, NativeSyntheticEvent, ScrollView, View } from 'react-native';
 
 const { width } = Dimensions.get('window');
 const aspectRatio = 1920 / 750;
@@ -11,21 +12,61 @@ const images = [
   sliderImages.banner4,
 ];
 
+const loopedImages = [images[images.length - 1], ...images, images[0]];
+
 const Slider = () => {
+  const scrollRef = useRef<ScrollView>(null);
+  const [currentIndex, setCurrentIndex] = useState(1);
+
+  useEffect(() => {
+    // Al montar, posiciona en la primera imagen real
+    scrollRef.current?.scrollTo({ x: width, animated: false });
+  }, []);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      let nextIndex = currentIndex + 1;
+      scrollRef.current?.scrollTo({ x: width * nextIndex, animated: true });
+    }, 4000);
+
+    return () => clearInterval(interval);
+  }, [currentIndex]);
+
+  const onMomentumScrollEnd = (e: NativeSyntheticEvent<NativeScrollEvent>) => {
+    let offsetX = e.nativeEvent.contentOffset.x;
+    let index = Math.round(offsetX / width);
+
+    if (index === 0) {
+      // Si está en la imagen clonada al inicio, salta a la última real
+      scrollRef.current?.scrollTo({ x: width * images.length, animated: false });
+      setCurrentIndex(images.length);
+    } else if (index === images.length + 1) {
+      // Si está en la imagen clonada al final, salta a la primera real
+      scrollRef.current?.scrollTo({ x: width, animated: false });
+      setCurrentIndex(1);
+    } else {
+      setCurrentIndex(index);
+    }
+  };
+        
+
   return (
     <ScrollView
+      ref={scrollRef}
       horizontal
       pagingEnabled
       showsHorizontalScrollIndicator={false}
       className="w-full"
+      onMomentumScrollEnd={onMomentumScrollEnd}
+      contentOffset={{ x: width, y: 0 }}
     >
-      {images.map((img, idx) => (
+      {loopedImages.map((img, idx) => (
         <View key={idx} style={{ width }}>
           <Image
             source={img}
             className="w-full rounded mb-2"
             style={{ height: width / aspectRatio, width: width }}
-            resizeMode="cover" 
+            resizeMode="cover"
           />
         </View>
       ))}
