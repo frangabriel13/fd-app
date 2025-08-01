f0e477abcfa7899131b6165ed2505a9ea8722328
Franco <mansilla.franco.1@gmail.com>
2025-08-01 13:18:10 -0300

    cambios

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,9 +1,10 @@
-import { StyleSheet, Text, View } from 'react-native';
+import Slider from '@/components/slider/Slider';
+import { StyleSheet, View } from 'react-native';
 
 const HomeScreen = () => {
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Fabricante Directo</Text>
+      <Slider />
     </View>
   );
 };
@@ -12,7 +13,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,                    // ESENCIAL: Ocupa todo el espacio disponible
     // backgroundColor: '#fff',
-    paddingHorizontal: 10,
+    // paddingHorizontal: 10,
   },
   text: {
     fontSize: 18,
--- components/slider/Slider.tsx
+++ components/slider/Slider.tsx
@@ -1,16 +1,20 @@
-import { StyleSheet, Text, View } from 'react-native';
+import sliderImages from '@/constants/Images';
+import { Image, StyleSheet, View } from 'react-native';
 
 const Slider = () => {
   return (
     <View style={styles.container}>
-      <Text>Slider</Text>
+      <Image source={sliderImages.banner1} />
+      <Image source={sliderImages.banner2} />
+      <Image source={sliderImages.banner3} />
+      <Image source={sliderImages.banner4} />
     </View>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
-    padding: 0,
+    paddingHorizontal: 0,
   },
 });
 
