3d1960b6f9c62d82ebbae7a766e6e9cbd49ba54d
Franco <mansilla.franco.1@gmail.com>
2025-08-01 13:49:06 -0300

    cambios

--- components/slider/Slider.tsx
+++ components/slider/Slider.tsx
@@ -1,13 +1,36 @@
 import sliderImages from '@/constants/Images';
-import { Image, StyleSheet, View } from 'react-native';
+import { Dimensions, Image, StyleSheet, View } from 'react-native';
+
+const { width } = Dimensions.get('window');
+const aspectRatio = 1920 / 750;
 
 const Slider = () => {
   return (
     <View style={styles.container}>
-      <Image source={sliderImages.banner1} />
-      <Image source={sliderImages.banner2} />
-      <Image source={sliderImages.banner3} />
-      <Image source={sliderImages.banner4} />
+      <Image 
+        source={sliderImages.banner1}
+        className='w-full rounded mb-2'
+        style={{ height: width / aspectRatio }}
+        resizeMode='cover'
+      />
+      <Image 
+        source={sliderImages.banner2}
+        className='w-full rounded mb-2'
+        style={{ height: width / aspectRatio }}
+        resizeMode='cover'
+      />
+      <Image 
+        source={sliderImages.banner3}
+        className='w-full rounded mb-2'
+        style={{ height: width / aspectRatio }}
+        resizeMode='cover'
+      />
+      <Image 
+        source={sliderImages.banner4}
+        className='w-full rounded mb-2'
+        style={{ height: width / aspectRatio }}
+        resizeMode='cover'
+      />
     </View>
   );
 };
