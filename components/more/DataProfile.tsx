745767d356136f065decaf7aaa9fc44d0c254be2
Franco <mansilla.franco.1@gmail.com>
2025-08-04 15:26:08 -0300

    cambios

--- components/more/DataProfile.tsx
+++ components/more/DataProfile.tsx
@@ -1,15 +1,26 @@
 import AntDesign from '@expo/vector-icons/AntDesign';
+import { StyleSheet, View } from 'react-native';
 import { BodyText, Card, H3 } from '../ui';
 
 const DataProfile = () => {
   return (
     <Card variant="default" className='bg-secondary'>
       <H3 className='text-white'>Franco Mansilla</H3>
-      <BodyText className='text-white'>
-        Mi perfil <AntDesign name="right" size={24} color="white" />
-      </BodyText>
+      <View style={styles.text}>
+        <BodyText className='text-white'>Mi perfil</BodyText>
+        <AntDesign name="right" size={18} color="white" style={{ marginLeft: 8 }} />
+      </View>
     </Card>
   );
 };
+
+const styles = StyleSheet.create({
+  text: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    cursor: 'pointer',
+  },
+});
+
 
 export default DataProfile;
