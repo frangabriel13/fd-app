3a56e8eb2f0b6749dc17bff63f7062b00af3e9c5
Franco <mansilla.franco.1@gmail.com>
2025-08-21 16:24:44 -0300

    cambios

--- app/(auth)/recuperar-password.tsx
+++ app/(auth)/recuperar-password.tsx
@@ -1,28 +1,50 @@
-import React from 'react';
+import { useState } from 'react';
 import { router } from 'expo-router';
-import { View, Text, StyleSheet } from 'react-native';
-import { Button } from '@/components/ui';
+import { TextInput } from 'react-native';
+import { Button, Container, H1 } from '@/components/ui';
 
 const ForgotPasswordScreen = () => {
+  const [email, setEmail] = useState('');
+  // const [loading, setLoading] = useState(false);
+
+
+
   return (
-    <View style={styles.container}>
-      <Text>Texto de ejemplo</Text>
+    <Container type="page" className="justify-center bg-primary">
+      <H1 className="text-center mb-8 text-white">Recuperar Contraseña</H1>
+
+      <TextInput
+        placeholder="Email"
+        value={email}
+        onChangeText={setEmail}
+        autoCapitalize="none"
+        keyboardType="email-address"
+        placeholderTextColor="#9CA3AF"
+        className="border border-gray-200 bg-white rounded-md px-4 py-3 mb-4 font-mont-regular text-gray-900"
+      />
+
       <Button 
-        variant="ghost" 
-        onPress={() => router.back()}
+        variant="primary" 
+        // onPress={handleLogin}
+        // loading={loading}
+        className="mb-4 rounded-md bg-secondary"
       >
-        ¿Ya tienes cuenta? Inicia sesión
+        Enviar
       </Button>
-    </View>
+
+      <Button 
+        variant="ghost"
+        // size='zero'
+        onPress={() => router.push('/(auth)/login')}
+        className="text-white"
+      >
+        Volver
+      </Button>
+    </Container>
   );
 };
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-});
+// const styles = StyleSheet.create({
+// });
 
 export default ForgotPasswordScreen;
