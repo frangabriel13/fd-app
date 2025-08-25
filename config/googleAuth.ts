29ba8e64a43bbb526e0262be2183114094bdc7f8
Franco <mansilla.franco.1@gmail.com>
2025-08-25 17:06:09 -0300

    cambios

--- config/googleAuth.ts
+++ config/googleAuth.ts
@@ -1,5 +1,5 @@
 export const GOOGLE_AUTH_CONFIG = {
-  androidClientId: 'tu-android-client-id.googleusercontent.com',
+  androidClientId: '378911157109-rfk3bon8k05gits799q8a6377469v00q.apps.googleusercontent.com',
   iosClientId: 'tu-ios-client-id.googleusercontent.com', 
-  webClientId: 'tu-web-client-id.googleusercontent.com',
+  webClientId: '378911157109-9n1bjfra29k7uv3qr78tdmdoaffja56b.apps.googleusercontent.com',
 };
