9d27906b2e3f5ba4982778c5276a052111f0ed11
Franco <mansilla.franco.1@gmail.com>
2025-09-23 16:16:43 -0300

    cambios

--- components/more/DataProfile.tsx
+++ components/more/DataProfile.tsx
@@ -1,17 +1,25 @@
 import AntDesign from '@expo/vector-icons/AntDesign';
-import { StyleSheet, View } from 'react-native';
+import { StyleSheet, View, Pressable } from 'react-native';
 import { BodyText, Card, H3 } from '../ui';
+import { useRouter } from 'expo-router';
 
 const DataProfile = () => {
+  const router = useRouter();
+
+  const handlePress = () => {
+    router.push('/(dashboard)/perfil');
+  }
+
   return (
-    // <Card variant="default" className='bg-secondary-400'>
-    <Card variant="default" className='bg-secondary-400'>
-      <H3 className='text-white'>Franco Mansilla</H3>
-      <View style={styles.text}>
-        <BodyText className='text-white'>Mi perfil</BodyText>
-        <AntDesign name="right" size={12} color="white" style={{ marginLeft: 2 }} />
-      </View>
-    </Card>
+    <Pressable onPress={handlePress}>
+      <Card variant="default" className='bg-secondary-400'>
+        <H3 className='text-white'>Franco Mansilla</H3>
+        <View style={styles.text}>
+          <BodyText className='text-white'>Mi perfil</BodyText>
+          <AntDesign name="right" size={12} color="white" style={{ marginLeft: 2 }} />
+        </View>
+      </Card>
+    </Pressable>
   );
 };
 
