dcec07acece19419a52568b860745ae938b93167
Franco <mansilla.franco.1@gmail.com>
2025-12-07 18:10:51 -0300

    cambios

---
+++ components/cart/ManufacturerCart.tsx
@@ -0,0 +1,18 @@
+import { View, Text, StyleSheet } from 'react-native';
+
+const ManufacturerCart = () => {
+  return (
+    <View style={styles.container}>
+      <Text>ManufacturerCart</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 0,
+  },
+});
+
+
+export default ManufacturerCart;
