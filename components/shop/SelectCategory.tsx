f6912cd89d54cdd2b87516096d530184d8743c5f
Franco <mansilla.franco.1@gmail.com>
2025-12-15 10:52:25 -0300

    cambios

--- components/shop/SelectCategory.tsx
+++ components/shop/SelectCategory.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, Image } from 'react-native';
 import { genders } from '@/utils/hardcode';
 
 type SelectCategoryProps = {
@@ -7,25 +7,49 @@ type SelectCategoryProps = {
 };
 
 const SelectCategory = ({ selectedGenderId }: SelectCategoryProps) => {
+  const selectedGender = genders.find(gender => gender.id === selectedGenderId);
+
   useEffect(() => {
-    const selectedGender = genders.find(gender => gender.id === selectedGenderId);
     if (selectedGender) {
       console.log(`Categorías de ${selectedGender.name}:`, selectedGender.categories);
     }
-  }, [selectedGenderId]);
+  }, [selectedGender]);
 
   return (
     <View style={styles.container}>
-      <Text>SelectCategory</Text>
+      {selectedGender?.categories.map((category, index) => (
+        <View key={index} style={styles.categoryContainer}>
+          <Image source={category.img} style={styles.image} />
+          <Text style={styles.categoryName}>{category.name}</Text>
+        </View>
+      ))}
     </View>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
-    padding: 0,
+    padding: 16,
+  },
+  title: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    marginBottom: 8,
+  },
+  categoryContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginBottom: 8,
+  },
+  image: {
+    width: 40,
+    height: 40,
+    borderRadius: 20,
+    marginRight: 12,
+  },
+  categoryName: {
+    fontSize: 16,
   },
 });
-
 
 export default SelectCategory;
