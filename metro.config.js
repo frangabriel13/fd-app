16094546b32875450c7063990d66cdcd465a427b
Franco <mansilla.franco.1@gmail.com>
2025-12-15 10:08:53 -0300

    cambios

--- metro.config.js
+++ metro.config.js
@@ -8,6 +8,11 @@
 const { getDefaultConfig } = require("expo/metro-config");
 const { withNativeWind } = require('nativewind/metro');
  
-const config = getDefaultConfig(__dirname)
- 
-module.exports = withNativeWind(config, { input: './app/global.css' })
+const config = getDefaultConfig(__dirname);
+
+// Añadir configuración para resolver el alias @
+config.resolver.alias = {
+  '@': __dirname,
+};
+
+module.exports = withNativeWind(config, { input: './app/global.css' });
