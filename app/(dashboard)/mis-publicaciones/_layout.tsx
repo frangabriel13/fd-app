6973aa791f99c086ce7410bd6bfac558d7b3cbd7
Franco <mansilla.franco.1@gmail.com>
2026-01-24 11:04:50 -0300

    cambios

---
+++ app/(dashboard)/mis-publicaciones/_layout.tsx
@@ -0,0 +1,25 @@
+import { Stack } from 'expo-router';
+import StepHeader from '@/components/headers/StepHeader';
+
+export default function PedidosLayout() {
+  return (
+    <Stack
+      screenOptions={{
+        contentStyle: { backgroundColor: "#f3f4f6" }
+      }}
+    >
+      <Stack.Screen
+        name="index"
+        options={{
+          header: () => <StepHeader title="Mis Publicaciones" />,
+        }}
+      />
+      <Stack.Screen
+        name="ver-pedido"
+        options={{
+          header: () => <StepHeader title="Editar Producto" />,
+        }}
+      />
+    </Stack>
+  )
+}
