0047d64b4c9974b425fd25f32dd13776cb4ae24b
Franco <mansilla.franco.1@gmail.com>
2026-03-25 20:39:15 -0300

    cambios

--- components/account/MenuAccount.tsx
+++ components/account/MenuAccount.tsx
@@ -67,6 +67,9 @@ const MenuAccount = ({ userRole }: MenuAccountProps) => {
       case 'Seguidos':
         router.push('/(tabs)/seguidos' as any);
         break;
+      case 'Favoritos':
+        router.push('/(tabs)/favoritos' as any);
+        break;
       default:
         Alert.alert('Acción', `Función: ${action}`, [{ text: 'OK' }]);
     }
